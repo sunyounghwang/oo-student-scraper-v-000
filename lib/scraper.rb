e631c1aecc75a06683913e1a1878f95d872b48dc
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-10-03 22:03:16 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,6 +10,7 @@ class Scraper
          name: student.css("h4.student-name").text,
          location: student.css("p.student-location").text,
          profile_url: student.css("a").attribute("href").value
+         binding.pry
        }
        index_array << student_hash
      end
